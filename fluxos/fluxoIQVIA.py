3e55aff53e27844f777bf5aa724f25cfa3cddb26
Mateus <mateussantoz522@gmail.com>
2026-04-10 16:56:32 -0300

    fluxo dos dados iqvia

---
+++ fluxos/fluxoIQVIA.py
@@ -0,0 +1,15 @@
+# Importações
+import pandas as pd
+from halo import Halo
+from prefect import flow
+
+# Importações das tasks
+from tasks.arquivos.carregamento import carregar_dados
+from tasks.tasks_iqvia.tratamento import tratar_dados_iqvia
+
+# Construnção do fluxo de IQVIA
+@flow(name = "Executando fluxo IQVIA")
+def fluxo_iqvia(caminho:str) -> pd.DataFrame:
+    dados = carregar_dados(caminho)
+    dados = tratar_dados_iqvia(dados)
+    return dados
