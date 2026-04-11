babc7907422dfa258614b890a72ae77b4480e83a
Mateus <mateussantoz522@gmail.com>
2026-04-11 16:53:19 -0300

    tarefa que classifica produtos no estoque a qualificar

---
+++ tasks/tasks_resultado/classificar.py
@@ -0,0 +1,10 @@
+# importações
+import pandas as pd
+from prefect import task
+
+# Criando fluxo para flag 
+# ideia principal é que a flag indique se o produto, está ou não no estoque a qualificar
+@task(name = "Classificando produtos")
+def flag(dados:pd.DataFrame, dados_estoque:pd.DataFrame) -> pd.DataFrame:
+    dados['Estoque.qualif'] = dados['EAN'].isin(dados_estoque['EAN'])
+    return dados
