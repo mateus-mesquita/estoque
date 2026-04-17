4a68073edbd47ecf1021dbc3e8634cbefcff7559
Mateus <mateussantoz522@gmail.com>
2026-04-17 10:33:39 -0300

    feat(metrics): ajusta filtros de performance e corrige erro de atributo no cálculo por molécula/apresentação

--- tasks/tasks_metricas/perfomance_mercado.py
+++ tasks/tasks_metricas/perfomance_mercado.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from prefect import task
+import numpy as np
 from funcoes.funcoes_axiliares import soma_ul_6_meses
 
 @task(name = "Calculando Perfomance - Mercado")
 def faturamento2025ME(dados:pd.DataFrame) -> pd.DataFrame:
     dados = soma_ul_6_meses(dados)
-    dados['Performance (2025) - Mercado'] = dados.groupby(['UF','PRODUTO','TIPO_FARMACIA'])['Soma ult.6 meses'].transform('sum')
+    
+    # Criando coluna de perfomance
+    filtro = (dados['UF'].isin(['CE','PI','MA']) & (dados['PRODUCT_DESC'].isin(list(dados['PRODUCT_DESC'].unique()))))
+    dados['Performance (2025) - Mercado'] = np.where(filtro, dados['Soma ult.6 meses'], 0)
     return dados
--- tasks/tasks_metricas/perfomance_molecula.py
+++ tasks/tasks_metricas/perfomance_molecula.py
@@ -1,20 +1,23 @@
 import pandas as pd
 from prefect import task
+import numpy as np
 from funcoes.funcoes_axiliares import soma_ul_6_meses
 
 
 @task(name = "Calculando a performance por  Molécula a Nível mercado")
 def performance_molecula_mercado(dados:pd.DataFrame) -> pd.DataFrame:
     dados = soma_ul_6_meses(dados)
-
-    # Calculando a performance por molécula
-    dados['Perfomance Molécula (2025) - Mercado'] = dados.groupby(['UF','TIPO_FARMACIA','MOLECULA'])['Soma ult.6 meses'].transform('sum')
+    
+    # Criando coluna de perfomance
+    filtro = ((dados['UF'].isin(['CE','PI','MA'])) & (dados['MOLECULA'].isin(list(dados['MOLECULA'].unique()))))
+    dados['molecula_Performance (2025) - Mercado'] = np.where(filtro, dados['Soma ult.6 meses'], 0)
     return dados
 
 @task(name = "Calculando a performance por  Molécula e Apresentação a Nível mercado")
 def performance_molecula_apr_mercado(dados:pd.DataFrame) -> pd.DataFrame:
     dados = soma_ul_6_meses(dados)
-
-    # Calculando a performance por molécula
-    dados['Performance Molecula e Apresentação (2025) - Mercado'] = dados.groupby(['UF','TIPO_FARMACIA','MOLECULA','apresentação'])['Soma ult.6 meses'].transform('sum')
+    
+    # Criando coluna de perfomance
+    filtro = ((dados['UF'].isin(['CE','PI','MA'])) & (dados['MOLECULA'].isin(list(dados['MOLECULA'].unique())) & (dados['apresentação'].isin(list(dados['apresentação'].unique())))))
+    dados['molecula_apr_Performance (2025) - Mercado'] = np.where(filtro, dados['Soma ult.6 meses'], 0)
     return dados
